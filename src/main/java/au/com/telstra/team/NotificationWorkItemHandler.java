ff71758cb5a722c3696cc598ccfea8b4058ce233
bcadmin <you@example.com>
2019-04-12 11:35:59 +1000

    added Handeler

---
+++ src/main/java/au/com/telstra/team/NotificationWorkItemHandler.java
@@ -0,0 +1,28 @@
+package au.com.telstra.team;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.springframework.stereotype.Component;
+
+@Component("Notification")
+public class NotificationWorkItemHandler implements WorkItemHandler {
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+        // extract parameters
+        String from = (String) workItem.getParameter("From");
+        String to = (String) workItem.getParameter("To");
+        String message = (String) workItem.getParameter("Message");
+        String priority = (String) workItem.getParameter("Priority");
+
+        System.out.println("NotificationWorkItemHandler log: " + from + " " + to + " " + message + " " + priority);
+
+        // notify manager that work item has been completed
+        workItemManager.completeWorkItem(workItem.getId(), null);
+
+    }
+
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+    }
+}
---
+++ target/classes/au/com/telstra/team/NotificationWorkItemHandler.java
@@ -0,0 +1,11 @@
+package au.com.telstra.team;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.springframework.stereotype.Component;
+
+@Component("MyTask")
+public class MyTaskWorkItemHandler implements WorkItemHandler {
+
+
+}
